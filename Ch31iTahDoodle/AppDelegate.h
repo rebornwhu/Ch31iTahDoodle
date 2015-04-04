231a379b62995ed4992a9a9cee66ab5be1f72352
Xiao Lu <xiaolu@Xiaos-MacBook-Pro.local>
2015-04-04 12:45:12 -0700

    Add some controllers

--- Ch31iTahDoodle/AppDelegate.h
+++ Ch31iTahDoodle/AppDelegate.h
@@ -8,9 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder
+<UIApplicationDelegate, UITableViewDataSource>
 
 @property (strong, nonatomic) UIWindow *window;
+
+@property (nonatomic) UITableView *taskTable;
+@property (nonatomic) UITextField *taskField;
+@property (nonatomic) UIButton *insertButton;
+
+@property (nonatomic) NSMutableArray *tasks;
+
+- (void)addTask:(id)sender;
 
 
 @end
--- Ch31iTahDoodle/AppDelegate.m
+++ Ch31iTahDoodle/AppDelegate.m
@@ -14,9 +14,45 @@
 
 @implementation AppDelegate
 
+#pragma - Application delegate callbacks
+
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    CGRect winFrame = [[UIScreen mainScreen] bounds];
+    UIWindow *theWindow = [[UIWindow alloc] initWithFrame:winFrame];
+    self.window = theWindow;
+    
+    CGRect tableFrame = CGRectMake(0, 80, winFrame.size.width, winFrame.size.height - 100);
+    CGRect fieldFrame = CGRectMake(20, 40, 200, 31);
+    CGRect buttonFrame = CGRectMake(228, 40, 72, 31);
+    
+    self.taskTable = [[UITableView alloc] initWithFrame:tableFrame style:UITableViewStylePlain];
+    self.taskTable.separatorStyle = UITableViewCellSeparatorStyleNone;
+    
+    [self.taskTable registerClass:[UITableViewCell class]
+           forCellReuseIdentifier:@"Cell"];
+
+    self.taskField = [[UITextField alloc] initWithFrame:fieldFrame];
+    self.taskField.borderStyle = UITextBorderStyleRoundedRect;
+    self.taskField.placeholder = @"Type a task, tap Insert";
+    
+    self.insertButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    self.insertButton.frame = buttonFrame;
+    
+    [self.insertButton setTitle:@"Insert"
+                       forState:UIControlStateNormal];
+    [self.insertButton addTarget:self
+                          action:@selector(addTask:)
+                forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    [self.window addSubview:self.taskTable];
+    [self.window addSubview:self.taskField];
+    [self.window addSubview:self.insertButton];
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
@@ -40,6 +76,22 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+
+#pragma mark - Actions
+- (void)addTask:(id)sender
+{
+    NSString *text = [self.taskField text];
+    
+    if ([text length] == 0) {
+        return;
+    }
+    
+    NSLog(@"Task entered: %@", text);
+    
+    [self.taskField setText:@""];
+    [self.taskField resignFirstResponder];
 }
 
 @end
